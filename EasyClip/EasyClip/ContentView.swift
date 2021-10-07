b5f01014e2451d4327b99870939509c1aee18de3
lihailong <lihailong@zhizhangyi.com>
2021-10-07 21:53:45 +0800

    init

--- EasyClip/EasyClip/ContentView.swift
+++ EasyClip/EasyClip/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        NavigationView(content: {
+            NavigationLink(destination: Text("Destination")) {
+                Text("Hello, World!")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+        })
     }
 }
 
